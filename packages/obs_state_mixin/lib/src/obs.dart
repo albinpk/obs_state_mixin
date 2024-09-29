011432921be651a0103b30946229b49ea801a8d8
Albin <56157868+albinpk@users.noreply.github.com>
2024-09-29 20:12:29 +0530

    fix: create `Obs` class

---
+++ packages/obs_state_mixin/lib/src/obs.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart';
+
+/// A class to hold a value and notify listeners when the value changes.
+///
+/// See also:
+///
+/// * [ValueNotifier], the class that [Obs] is based on.
+class Obs<T> extends ValueNotifier<T> {
+  /// Creates a new [Obs] with the given [value].
+  Obs(super.value);
+
+  /// A string representation of the [Obs].
+  @override
+  String toString() => value.toString();
+}
