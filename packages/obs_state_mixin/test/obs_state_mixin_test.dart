10f5dedfbe7c4859ac067cb891acdd89ae058c65
Albin <56157868+albinpk@users.noreply.github.com>
2024-10-02 18:10:51 +0530

    test: create widget test for obs_state_mixin

--- packages/obs_state_mixin/test/obs_state_mixin_test.dart
+++ packages/obs_state_mixin/test/obs_state_mixin_test.dart
@@ -1,7 +1,90 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:obs_state_mixin/obs_state_mixin.dart';
 
 void main() {
-  test('test ...', () {
-    expect(1 + 1, 2);
+  testWidgets('obs updates', (tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(home: _TestWidget()),
+    );
+
+    expect(find.text('0'), findsOne);
+
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    expect(find.text('1'), findsOne);
   });
+
+  testWidgets('obs auto dispose', (tester) async {
+    late Obs<int> obs;
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: _AutoDisposeTestWidget(
+          fn: (Obs<int> value) {
+            obs = value;
+          },
+        ),
+      ),
+    );
+
+    expect(obs.value, 0);
+
+    // disposing the previous widget, it also disposes the obs
+    await tester.pumpWidget(const SizedBox());
+
+    // updating the obs value will throw, since the widget has been disposed
+    expect(() => obs.value++, throwsFlutterError);
+  });
+}
+
+class _TestWidget extends StatefulWidget {
+  const _TestWidget();
+
+  @override
+  State<_TestWidget> createState() => _TestWidgetState();
+}
+
+class _TestWidgetState extends State<_TestWidget> with ObsStateMixin {
+  late final counter = obs(0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('$counter'),
+          IconButton(
+            onPressed: () => counter.value++,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _AutoDisposeTestWidget extends StatefulWidget {
+  const _AutoDisposeTestWidget({required this.fn});
+
+  final void Function(Obs<int> obs) fn;
+
+  @override
+  State<_AutoDisposeTestWidget> createState() => _AutoDisposeTestWidgetState();
+}
+
+class _AutoDisposeTestWidgetState extends State<_AutoDisposeTestWidget> with ObsStateMixin {
+  late final counter = obs(0);
+
+  @override
+  void initState() {
+    super.initState();
+    widget.fn(counter);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('$counter');
+  }
 }
