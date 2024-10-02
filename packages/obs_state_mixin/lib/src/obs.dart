7b452f429ed93b322212483994a1f1ca8d4f177b
Albin <56157868+albinpk@users.noreply.github.com>
2024-10-02 21:13:35 +0530

    feat: add `refresh` method in `Obs`

--- packages/obs_state_mixin/lib/src/obs.dart
+++ packages/obs_state_mixin/lib/src/obs.dart
@@ -9,6 +9,9 @@ class Obs<T> extends ValueNotifier<T> {
   /// Creates a new [Obs] with the given [value].
   Obs(super.value);
 
+  /// Notifies listeners that the value has changed.
+  void refresh() => notifyListeners();
+
   /// A string representation of the [Obs].
   @override
   String toString() => value.toString();
