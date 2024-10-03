0b3c3174df821fef04e0ecc44dc7749ff560d396
Albin <56157868+albinpk@users.noreply.github.com>
2024-10-03 21:34:15 +0530

    fix: create extensions for `Obs<Iterable>`

--- packages/obs_state_mixin/lib/obs_state_mixin.dart
+++ packages/obs_state_mixin/lib/obs_state_mixin.dart
@@ -1,6 +1,7 @@
 library obs_state_mixin;
 
 export 'src/extensions/obs_bool_extension.dart';
+export 'src/extensions/obs_iterable_extension.dart';
 export 'src/extensions/obs_list_extension.dart';
 export 'src/extensions/obs_num_extension.dart';
 export 'src/extensions/obs_string_extension.dart';
