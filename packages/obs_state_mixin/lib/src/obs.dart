d867de57e74eb1dc86f3f348bd7505f86f1505ae
Albin <56157868+albinpk@users.noreply.github.com>
2024-10-13 16:10:36 +0530

    feat: create `setValue` method in `Obs`

--- packages/obs_state_mixin/lib/src/obs.dart
+++ packages/obs_state_mixin/lib/src/obs.dart
@@ -9,6 +9,35 @@ class Obs<T> extends ValueNotifier<T> {
   /// Creates a new [Obs] with the given [value].
   Obs(super.value);
 
+  /// Sets the [value] of the [Obs] to [newValue].
+  ///
+  /// ```dart
+  /// final count = Obs(0);
+  /// count.setValue(1); // Same as count.value = 1;
+  /// ```
+  ///
+  /// This is useful when you want to update the value of the [Obs] in a callback,
+  /// such as when responding to user input in a [TextField].
+  ///
+  /// eg.
+  ///
+  /// ```dart
+  /// final userName = obs('');
+  ///
+  /// // Using callback
+  /// TextField(
+  ///   onChanged: (input) {
+  ///     userName.value = input;
+  ///   },
+  /// );
+  ///
+  /// // Using setValue method
+  /// TextField(
+  ///   onChanged: userName.setValue,
+  /// );
+  /// ```
+  void setValue(T newValue) => value = newValue;
+
   /// Notifies listeners that the value has changed.
   void refresh() => notifyListeners();
 
